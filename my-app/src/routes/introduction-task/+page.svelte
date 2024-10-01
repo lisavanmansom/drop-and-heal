d032887e956c0628f6ae5e2e9160a415a1cae401
lisavanmansom <lisavanmansom@gmail.com>
2024-10-01 09:19:38 +0200

    update w api

--- my-app/src/routes/introduction-task/+page.svelte
+++ my-app/src/routes/introduction-task/+page.svelte
@@ -1,30 +1,36 @@
 <script>
-    	import MeshgradBlue from '$lib/meshgrad-blue.svelte';
-        import MeshgradRed from '$lib/meshgrad-red.svelte';
-        import MeshgradGreen from '$lib/meshgrad-green.svelte';
-        import MeshgradPink from '$lib/meshgrad-pink.svelte';
-        import ArrowL from '$lib/arrow-l.svelte';
-        import ArrowR from '$lib/arrow-r.svelte';
+  export let task;
+  console.log('Task:', task);
+
+  function stripHtml(html) {
+    return html.replace(/<\/?[^>]+(>|$)/g, "");
+    // Verwijderd HTML tags die meegegeven zijn vanuit directus
+  }
+
+  import {MeshgradBlue, MeshgradRed, MeshgradGreen, MeshgradPink, ArrowL, ArrowR} from '$lib'
+
 
 </script>
 
 <main>
-    <header>
+    <section>
         <h1>Introductie rouwtaken</h1>
-    </header>
+    </section>
 
-    <section id="rt-1">
+      <section id="rt-1">
         <h2>Rouwtaak <em>1</em></h2>
         <h3>Het verlies aanvaarden</h3>
-        <p>Ontdek hoe je de realiteit van het verlies kunt omarmen.</p>
+        <p>Ontedek hoe je de realiteit van het verlies kunt omarmen.</p> 
         <MeshgradBlue />
 
         <div aria-busy="true" aria-describedby="progress-bar"></div>
         <progress value="20" max="100"></progress>
 
-        <a href="#rt-2">
-            <ArrowR /></a>
-    </section>
+        <a href="#rt-1">
+          <ArrowR />
+        </a>
+      </section>
+
 
     <section id="rt-2">
         <h2>Rouwtaak <em>2</em></h2>
@@ -83,6 +89,43 @@
 
     em {
         font-family: dm-serif;
+    }
+
+    main {
+        align-items: center;
+        background-color: var(--b);
+        color: var(--w);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        overflow: hidden;
+        min-height: 100vh;
+        width: 100%;
+    }
+
+    section {
+        min-height: 100vh;
+    }
+
+    section a {
+        padding: 1.2em 0.7em 0.5em 0.7em;
+        border-radius: 50%;
+    }
+
+    #rt-1 a {
+        border: .2em solid var(--rt-1);
+    }
+
+    #rt-2 a {
+        border: .2em solid var(--rt-2);
+    }
+
+    #rt-3 a {
+        border: .2em solid var(--rt-3);
+    }
+
+    #rt-4 a {
+        border: .2em solid var(--rt-4);
     }
 
 </style>
