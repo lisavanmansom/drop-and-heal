c32253bf492bb24ffdc8ac2819927d4d548db3ea
TristanKatte <tristankatte@hotmail.com>
2024-10-03 00:52:02 +0200

    .

--- my-app/src/routes/chatbot/+page.svelte
+++ my-app/src/routes/chatbot/+page.svelte
@@ -7,8 +7,13 @@
       newMessage = '';
     }
   </script>
+
+  <main>
+    <section class="msg-header">
+      <h2>Chat</h2>
+    </section>
   
-  <div class="chatroom">
+  <section class="chatroom">
     <h1>Chatroom</h1>
     <ul>
       {#each messages as message}
@@ -17,9 +22,39 @@
     </ul>
     <input type="text" bind:value={newMessage} placeholder="Type je bericht"/>
     <button on:click={sendMessage}>Verstuur</button>
-  </div>
+  </section>
+
+  <section class="msg-bottom">
+               
+              
+  </section> 
+  
+
+
+</main>
   
   <style>
+
+    main {
+       width: 80%;
+       margin: auto;
+       margin-top: 2rem;
+       letter-spacing: 0.5px;
+       height: 80%;
+    }
+
+    .msg-header {
+      border: 1px solid #ccc;
+      width: 100%;
+      height: 10%;
+      border-bottom: none;
+      display: inline-block;
+      background-color: #efefef;
+      margin: 0;
+      border-top-left-radius: 20px;
+      border-top-right-radius: 20px;
+    }
+    
     .chatroom {
       max-width: 100%;
       height: 100vh;
